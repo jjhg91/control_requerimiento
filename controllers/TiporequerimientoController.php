e1778f74d488d2e246d6751282d2865ad1b5adc2
Juan Herrera <jjhg91@gmail.com>
2021-05-31 12:47:03 -0400

    modificando error en linux TipoRequerimientoSearch

--- controllers/TiporequerimientoController.php
+++ controllers/TiporequerimientoController.php
@@ -4,7 +4,7 @@ namespace app\controllers;
 
 use Yii;
 use app\models\TipoRequerimiento;
-use app\models\TiporequerimientoSearch;
+use app\models\TipoRequerimientoSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
